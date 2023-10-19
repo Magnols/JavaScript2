dd7f9055a9310f62d27b0ee6f4ffc96db66b8612
Magno Silva <131492865+Magnols@users.noreply.github.com>
2023-10-19 13:20:46 -0300

    código mais conciso

--- #1DaysOfCode.js
+++ #1DaysOfCode.js
@@ -5,20 +5,14 @@ let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
 
-if (numeroUm == stringUm) {
-    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
-} else {
-    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
-}
+const variaveis = [numeroUm, stringUm, numeroTrinta, stringTrinta, numeroDez, stringDez];
 
-if (numeroTrinta === Number(stringTrinta)) {
-    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
-} else {
-    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
-}
-
-if (numeroDez == stringDez) {
-    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
-} else {
-    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+for (let i = 0; i < variaveis.length; i++) {
+    for (let j = i + 1; j < variaveis.length; j++) {
+        if (variaveis[i] == variaveis[j]) {
+            console.log(`As variáveis ${variaveis[i]} e ${variaveis[j]} têm o mesmo valor, mas tipos diferentes`);
+        } else {
+            console.log(`As variáveis ${variaveis[i]} e ${variaveis[j]} não têm o mesmo valor`);
+        }
+    }
 }
