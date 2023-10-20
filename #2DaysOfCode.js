c748b8103fd9d7df8aa0957d6a1c07ecee8726a5
Magno Silva <131492865+Magnols@users.noreply.github.com>
2023-10-20 13:32:09 -0300

    Versão 01

--- #2DaysOfCode.js
+++ #2DaysOfCode.js
@@ -1,5 +1,5 @@
-let nome = prompt("Qual é o seu nome?");
-let idade = prompt("Quantos anos você tem?");
-let linguagem = prompt("Qual linguagem de programação você está estudando?");
+var nome = prompt("Qual é o seu nome?");
+var idade = prompt("Quantos anos você tem?");
+var linguagem = prompt("Qual linguagem de programação você está estudando?");
 
-alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+console.log("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
