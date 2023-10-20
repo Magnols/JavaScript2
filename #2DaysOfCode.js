ceab30e53c0b2989e8eaae497716c3b85d7a939d
Magno Silva <131492865+Magnols@users.noreply.github.com>
2023-10-20 13:48:53 -0300

    Update #2DaysOfCode.js

--- #2DaysOfCode.js
+++ #2DaysOfCode.js
@@ -1,5 +1,15 @@
-var nome = prompt("Qual é o seu nome?");
-var idade = prompt("Quantos anos você tem?");
-var linguagem = prompt("Qual linguagem de programação você está estudando?");
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está estudando?");
 
 console.log("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
+
+let resposta = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
+
+if (resposta === "1") {
+    console.log("Muito bom! Continue estudando e você terá muito sucesso.");
+} else if (resposta === "2") {
+    console.log("Ahh que pena... Já tentou aprender outras linguagens?");
+} else {
+    console.log("Resposta inválida. Por favor, responda com 1 para SIM ou 2 para NÃO.");
+}
