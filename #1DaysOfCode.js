78759b795a48491eafc94943490ef3a75c980a57
Magno Silva <131492865+Magnols@users.noreply.github.com>
2023-10-20 10:26:06 -0300

    Versão final do desafio

--- #1DaysOfCode.js
+++ #1DaysOfCode.js
@@ -5,31 +5,17 @@ let stringTrinta = '30';
 let numeroDez = 10;
 let stringDez = '10';
 
-const variaveis = [
-    { nome: 'numeroUm', valor: numeroUm },
-    { nome: 'stringUm', valor: stringUm },
-    { nome: 'numeroTrinta', valor: numeroTrinta },
-    { nome: 'stringTrinta', valor: stringTrinta },
-    { nome: 'numeroDez', valor: numeroDez },
-    { nome: 'stringDez', valor: stringDez },
-];
+let variaveis = [numeroUm, stringUm, numeroTrinta, stringTrinta, numeroDez, stringDez];
+let nomesVariaveis = ["numeroUm", "stringUm", "numeroTrinta", "stringTrinta", "numeroDez", "stringDez"];
 
 for (let i = 0; i < variaveis.length; i++) {
     for (let j = i + 1; j < variaveis.length; j++) {
-        const variavelA = variaveis[i];
-        const variavelB = variaveis[j];
-
-        const valorIgual = variavelA.valor == variavelB.valor;
-        const tipoIgual = typeof variavelA.valor === typeof variavelB.valor;
-
-        if (valorIgual && tipoIgual) {
-            console.log(`As variáveis ${variavelA.nome} e ${variavelB.nome} têm o mesmo valor e o mesmo tipo`);
-        } else if (valorIgual && !tipoIgual) {
-            console.log(`As variáveis ${variavelA.nome} e ${variavelB.nome} têm o mesmo valor, mas tipos diferentes`);
-        } else if (!valorIgual && tipoIgual) {
-            console.log(`As variáveis ${variavelA.nome} e ${variavelB.nome} têm valores diferentes, mas o mesmo tipo`);
+        if (variaveis[i] === variaveis[j]) {
+            console.log(`As variáveis ${nomesVariaveis[i]} e ${nomesVariaveis[j]} têm o mesmo valor e mesmo tipo.`);
+        } else if (variaveis[i] == variaveis[j]) {
+            console.log(`As variáveis ${nomesVariaveis[i]} e ${nomesVariaveis[j]} têm o mesmo valor, mas tipos diferentes.`);
         } else {
-            console.log(`As variáveis ${variavelA.nome} e ${variavelB.nome} têm valores e tipos diferentes`);
+            console.log(`As variáveis ${nomesVariaveis[i]} e ${nomesVariaveis[j]} têm valores e tipos diferentes.`);
         }
     }
 }
