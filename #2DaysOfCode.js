89f86a1055778388e43b52f0ff3e3f36a3c18d1b
Magno Silva <131492865+Magnols@users.noreply.github.com>
2023-10-20 17:14:12 -0300

    Mudança de Console.log para alert e mudança no else

--- #2DaysOfCode.js
+++ #2DaysOfCode.js
@@ -2,14 +2,14 @@ let nome = prompt("Qual é o seu nome?");
 let idade = prompt("Quantos anos você tem?");
 let linguagem = prompt("Qual linguagem de programação você está estudando?");
 
-console.log("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
+alert("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
 
 let resposta = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
 
 if (resposta === "1") {
-    console.log("Muito bom! Continue estudando e você terá muito sucesso.");
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
 } else if (resposta === "2") {
-    console.log("Ahh que pena... Já tentou aprender outras linguagens?");
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
 } else {
-    console.log("Resposta inválida. Por favor, responda com 1 para SIM ou 2 para NÃO.");
+    alert("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
 }
