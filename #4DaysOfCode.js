d1a44cbcbc53b359a5b55138d4141e93b65a8524
Magno Silva <131492865+Magnols@users.noreply.github.com>
2023-10-24 10:42:27 -0300

    desafio 4 parte 1 e deixando apenas os códigos js

---
+++ #4DaysOfCode.js
@@ -0,0 +1,23 @@
+const numeroSecreto = 7;
+let tentativas = 0;
+
+while (tentativas < 3) {
+
+    const tentativa = prompt("Chute um número de 0 a 10:");
+
+
+    const numeroTentativa = parseInt(tentativa);
+
+
+    if (numeroTentativa === numeroSecreto) {
+        alert("Parabéns! Você acertou o número.");
+        break;
+    } else {
+        alert("Tente novamente.");
+        tentativas++;
+    }
+}
+
+if (tentativas === 3) {
+    alert(`Você não conseguiu acertar. O número secreto era ${numeroSecreto}.`);
+}
